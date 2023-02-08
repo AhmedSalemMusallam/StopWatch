63d686d2f6655f2ed5a3beafa501bc1baa7f7fc9
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2023-02-08 16:29:07 +0200

    StopWatch Project Creation - Commit

---
+++ StopWatch/StopWatch/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Ahmed Salem on 08/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
